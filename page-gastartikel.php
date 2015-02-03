608831b2f1cc22843bc46019c58f786fbf5647d5
mvandek <maarten@mvandek.nl>
2015-02-03 13:22:28 +0100

    Update page-gastartikel.php
    Class page-gastartikel veranderd naar page gastartikel, zodat de CSS voor .has-post-thumbnail goed toegepast wordt

--- page-gastartikel.php
+++ page-gastartikel.php
@@ -7,7 +7,7 @@
 */
 get_header(); ?>
 <main class="content" role="main" itemscope="itemscope" itemtype="http://schema.org/Blog">
-<div class="entry page-gastartikel">
+<div class="entry page gastartikel">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php if ( function_exists('breadcrumb_trail') ) {
 breadcrumb_trail();
