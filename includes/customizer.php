498cdf03a839d98671bb79058d2a20b35d2c5c90
Maarten van de Kamp <maarten@mvandek.nl>
2015-02-11 12:37:52 +0100

    Update customizer.php
    Functies hernoemt van esplanade naar mvdk, verder de sanitize_callbak
    bij elke add_setting toegevoegd, in plaats van bij add_control.

--- includes/customizer.php
+++ includes/customizer.php
@@ -2,105 +2,120 @@
 /*******************************************************************
 * These are settings for the Theme Customizer in the admin panel.
 *******************************************************************/
-function esplanade_theme_customizer( $wp_customize ) {
+function mvdk_theme_customizer( $wp_customize ) {
 
 /* social media option */
 $wp_customize->add_section( 'esplanade_social_section' , array(
-'title'       => __( 'Social Media', 'esplanade' ),
+'title'       => __( 'Social Media', 'mvdk' ),
 'priority'    => 110,
-'description' => __( 'Voeg Social Media knoppen toe in de header van de website', 'esplanade' ),
+'description' => __( 'Voeg Social Media knoppen toe in de header van de website', 'mvdk' ),
 ) );
 
-$wp_customize->add_setting( 'esplanade_facebook' );
+$wp_customize->add_setting( 'esplanade_facebook', array(
+'default'           => '',
+'sanitize_callback' => 'mvdk_sanitize_url',
+) );
+
 $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'esplanade_facebook', array(
-'label'    => __( 'URL van Facebook pagina', 'esplanade' ),
+'label'    => __( 'URL van Facebook pagina', 'mvdk' ),
 'section'  => 'esplanade_social_section',
 'settings' => 'esplanade_facebook',
-'sanitize_callback' => 'mvdk_sanitize_url',
 'priority'    => 111,
 ) ) );
 
-$wp_customize->add_setting( 'esplanade_twitter' );
+$wp_customize->add_setting( 'esplanade_twitter', array(
+'default'           => '',
+'sanitize_callback' => 'mvdk_sanitize_url',
+) );
+
 $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'esplanade_twitter', array(
-'label'    => __( 'URL van Twitter pagina', 'esplanade' ),
+'label'    => __( 'URL van Twitter pagina', 'mvdk' ),
 'section'  => 'esplanade_social_section',
 'settings' => 'esplanade_twitter',
-'sanitize_callback' => 'mvdk_sanitize_url',
 'priority'    => 112,
 ) ) );
 
-$wp_customize->add_setting( 'esplanade_500px' );
+$wp_customize->add_setting( 'esplanade_500px', array(
+'default'           => '',
+'sanitize_callback' => 'mvdk_sanitize_url',
+) );
+
 $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'esplanade_500px', array(
-'label'    => __( 'URL van 500px.com pagina', 'esplanade' ),
+'label'    => __( 'URL van 500px.com pagina', 'mvdk' ),
 'section'  => 'esplanade_social_section',
 'settings' => 'esplanade_500px',
-'sanitize_callback' => 'mvdk_sanitize_url',
 'priority'    => 113,
 ) ) );
 
-$wp_customize->add_setting( 'esplanade_linkedin' );
+$wp_customize->add_setting( 'esplanade_linkedin', array(
+'default'           => '',
+'sanitize_callback' => 'mvdk_sanitize_url',
+) );
+
 $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'esplanade_linkedin', array(
-'label'    => __( 'URL van Linkedin pagina', 'esplanade' ),
+'label'    => __( 'URL van Linkedin pagina', 'mvdk' ),
 'section'  => 'esplanade_social_section',
 'settings' => 'esplanade_linkedin',
-'sanitize_callback' => 'mvdk_sanitize_url',
 'priority'    => 114,
 ) ) );
 
-$wp_customize->add_setting( 'esplanade_flickr' );
+$wp_customize->add_setting( 'esplanade_flickr', array(
+'default'           => '',
+'sanitize_callback' => 'mvdk_sanitize_url',
+) );
+
 $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'esplanade_flickr', array(
-'label'    => __( 'URL van Flickr pagina', 'esplanade' ),
+'label'    => __( 'URL van Flickr pagina', 'mvdk' ),
 'section'  => 'esplanade_social_section',
 'settings' => 'esplanade_flickr',
-'sanitize_callback' => 'mvdk_sanitize_url',
 'priority'    => 115,
 ) ) );
 
 $wp_customize->add_section( 'esplanade_theme_options', array(
-'title'             => __( 'Custom Post Type Page Options', 'esplanade' ),
+'title'             => __( 'Custom Post Type Page Options', 'mvdk' ),
 'priority'          => 120,
 ) );
 
 $wp_customize->add_setting( 'mvdk_hide_advertentie_page_content', array(
 'default'           => '',
-'sanitize_callback' => 'esplanade_sanitize_checkbox',
+'sanitize_callback' => 'mvdk_sanitize_checkbox',
 ) );
 
 $wp_customize->add_control( 'mvdk_hide_advertentie_page_content', array(
-'label'             => __( 'Hide title and content on Advertentie Page Template', 'esplanade' ),
+'label'             => __( 'Hide title and content on Advertentie Page Template', 'mvdk' ),
 'section'           => 'esplanade_theme_options',
 'type'              => 'checkbox',
 ) );
 
 $wp_customize->add_setting( 'mvdk_hide_gastartikel_page_content', array(
 'default'           => '',
-'sanitize_callback' => 'esplanade_sanitize_checkbox',
+'sanitize_callback' => 'mvdk_sanitize_checkbox',
 ) );
 
 $wp_customize->add_control( 'mvdk_hide_gastartikel_page_content', array(
-'label'             => __( 'Hide title and content on Gastartikel Page Template', 'esplanade' ),
+'label'             => __( 'Hide title and content on Gastartikel Page Template', 'mvdk' ),
 'section'           => 'esplanade_theme_options',
 'type'              => 'checkbox',
 ) );
 
 $wp_customize->add_setting( 'mvdk_hide_portfolio_page_content', array(
 'default'           => '',
-'sanitize_callback' => 'esplanade_sanitize_checkbox',
+'sanitize_callback' => 'mvdk_sanitize_checkbox',
 ) );
 
 $wp_customize->add_control( 'mvdk_hide_portfolio_page_content', array(
-'label'             => __( 'Hide title and content on Portfolio Page Template', 'esplanade' ),
+'label'             => __( 'Hide title and content on Portfolio Page Template', 'mvdk' ),
 'section'           => 'esplanade_theme_options',
 'type'              => 'checkbox',
 ) );
 
 $wp_customize->add_setting( 'mvdk_hide_workshop_page_content', array(
 'default'           => '',
-'sanitize_callback' => 'esplanade_sanitize_checkbox',
+'sanitize_callback' => 'mvdk_sanitize_checkbox',
 ) );
 
 $wp_customize->add_control( 'mvdk_hide_workshop_page_content', array(
-'label'             => __( 'Hide title and content on Workshop Page Template', 'esplanade' ),
+'label'             => __( 'Hide title and content on Workshop Page Template', 'mvdk' ),
 'section'           => 'esplanade_theme_options',
 'type'              => 'checkbox',
 ) );
@@ -114,25 +129,25 @@ $wp_customize->add_section( 'mvdk_footer_settings', array(
 // Customize Footer Text
 $wp_customize->add_setting( 'mvdk_custom_footer_text', array(
 'default'        => ' ',
+'sanitize_callback' => 'mvdk_sanitize_text',
 ) );
 
 $wp_customize->add_control( 'mvdk_custom_footer_text', array(
-'label'   => __('Footer tekst','matt2015'),
+'label'   => __('Footer tekst','mvdk'),
 'section' => 'mvdk_footer_settings',
-'sanitize_callback' => 'mvdk_sanitize_text',
 'type'    => 'text',
 'transport' => 'refresh',
 ) );
 
 }
-add_action('customize_register', 'esplanade_theme_customizer');
+add_action('customize_register', 'mvdk_theme_customizer');
 /**
  * Sanitize the checkbox.
  *
  * @param boolean $input.
  * @return boolean true if portfolio page template displays title and content.
  */
-function esplanade_sanitize_checkbox( $input ) {
+function mvdk_sanitize_checkbox( $input ) {
 if ( 1 == $input ) {
 return true;
 } else {
@@ -145,5 +160,5 @@ return esc_url( $input );
 }
 
 function mvdk_sanitize_text( $input ) {
-return wp_kses_post( force_balance_tags( $input ) );
+return esc_html( $input );
 }
