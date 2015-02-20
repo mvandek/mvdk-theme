012c14f34fc94b464c25d1bf10a044296eab22ec
mvandek <maarten@mvandek.nl>
2015-02-20 13:31:41 +0100

    Update template-tags.php
    Added the_posts_navigation for future use in theme, and replacement of mvdk_paging_nav()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -32,6 +32,30 @@ return;
 <?php
 }
 /**
+ * Display navigation to next/previous set of posts when applicable.
+ *
+ * @todo Remove this function when WordPress 4.3 is released.
+ */
+function the_posts_navigation() {
+// Don't print empty markup if there's only one page.
+if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+return;
+}
+?>
+<nav class="navigation posts-navigation" role="navigation">
+<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'sobe' ); ?></h2>
+<div class="nav-links">
+<?php if ( get_next_posts_link() ) : ?>
+<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'sobe' ) ); ?></div>
+<?php endif; ?>
+<?php if ( get_previous_posts_link() ) : ?>
+<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'sobe' ) ); ?></div>
+<?php endif; ?>
+</div><!-- .nav-links -->
+</nav><!-- .navigation -->
+<?php
+}
+/**
 * Displays navigation to next/previous post when applicable.
 *
 *
