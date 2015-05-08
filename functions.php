8a48e473fdbf8da9153365f75e74dc9d0518680a
Maarten van de Kamp <maarten@mvandek.nl>
2015-05-08 09:41:43 +0200

    Update functions.php and create navigation.js
    For tabbable dropdown menus

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ wp_dequeue_script( 'twemoji' );
 // Enable jQuery.
 wp_enqueue_script( 'jquery' );
 // Loads JavaScript files
-wp_enqueue_script( 'responsive-menu-script', get_template_directory_uri() . '/js/responsive-menu.js', ['jquery'], '20150101', true );
+wp_enqueue_script( 'navigation-script', get_template_directory_uri() . '/js/navigation.js', ['jquery'], '20150101', true );
 wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', [], '20150218', true );
 if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
 wp_enqueue_script( 'comment-reply' );
